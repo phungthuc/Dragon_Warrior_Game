e733e2e1c4feec89690b4fa2ccee2a7eaa16b880
ThucPhung <phungthuc39@gmail.com>
2023-07-12 17:31:00 +0700

    + Create Logic Handle collision In Collider

---
+++ src/physics/collision/collider.js
@@ -0,0 +1,20 @@
+import { Container, Texture } from "pixi.js";
+import { GameConstant } from "../../gameConstant";
+
+export class Collider extends Container {
+    constructor(tag, x, y, width, height) {
+        super(Texture.WHITE);
+        this.tag = tag;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.enable = true;
+
+        this.visible = GameConstant.DEBUG_COLLIDER;
+    }
+
+    getBounding() {
+        return this.getBounds();
+    }
+}
