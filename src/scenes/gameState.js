ff0b29e14662fd3a74565c7e11e0f37df72e4e7d
ThucPhung <phungthuc39@gmail.com>
2023-07-12 10:04:43 +0700

    + Create Game State

---
+++ src/scenes/gameState.js
@@ -0,0 +1,8 @@
+export const GameState = Object.freeze({
+    Start: "gamestate:start",
+    Playing: "gamestate:playing",
+    WinLevel: "gamestate:losslevel",
+    LossLevel: "gamestate:losslevel",
+    Win: "gamestate:win",
+    Loss: "gamestate:loss"
+});
