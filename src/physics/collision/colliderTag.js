ec38437d5c5ad38e2ff5cbb5edf1ee497ea3d324
ThucPhung <phungthuc39@gmail.com>
2023-07-12 17:26:38 +0700

    + Create Collider Tag File

---
+++ src/physics/collision/colliderTag.js
@@ -0,0 +1,8 @@
+export const ColliderTag = Object.freeze({
+    Dragon: "collidertag:dragon",
+    DragonFire: "collidertag:dragonfire",
+    PipeTop: "collidertag:pipetop",
+    PipeBottom: "collidertag:pipebotom",
+    Boss: "collidertag:boss",
+    BossFire: "collidertag:bossfire"
+});
