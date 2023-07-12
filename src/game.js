95dab8c394072bc14338a4a411b8470ba4394710
ThucPhung <phungthuc39@gmail.com>
2023-07-12 10:01:17 +0700

    - Change Scale Screen

--- src/game.js
+++ src/game.js
@@ -1,5 +1,7 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets } from "pixi.js";
 import { GameConstant } from "./gameConstant";
+import { manifest } from "./manifest/assets";
+import { PlayScene } from "./scenes/playScene";
 
 export class Game {
     static init() {
@@ -15,28 +17,51 @@ export class Game {
         viewStyle.padding = "0px 0px 0px 0px";
         this.resize(window.innerWidth, window.innerHeight);
 
+        this._loadGameAssets().then(() => {
+            this._initScene();
+            this.app.ticker.add(this.update, this);
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 
     static resize(width, height) {
         let style = this.app.view.style;
         this.windowWidth = width;
         this.windowHeight = height;
-        let ratio = Math.max(GameConstant.GAME_WIDTH / this.windowWidth, GameConstant.GAME_HEIGHT / this.windowHeight);
-        this.width = this.windowWidth * ratio;
-        this.height = this.windowHeight * ratio;
-        this.app.view.width = this.width;
-        this.app.view.height = this.height;
-        let scale = this.windowWidth / this.width;
-        style.transformOrigin = "0px 0px";
-        style.transform = `scale(${scale})`;
-        let vMargin = Math.floor((this.windowWidth - this.width * scale) / 2);
-        let hMargin = Math.floor((this.windowHeight - this.height * scale) / 2);
-
-        style.margin = `${hMargin}px ${vMargin}px ${hMargin}px ${vMargin}px`;
+        this.app.view.width = this.windowWidth;
+        this.app.view.height = this.windowHeight;
         this.app.resizeTo = this.app.view;
         this.app.resize();
 
         this.playScene && this.playScene.resize();
+    }
+
+    static async _loadGameAssets() {
+        await Assets.init({
+            manifest: manifest
+        });
+
+        const bundleIds = manifest.bundles.map(
+            bundles => bundles.name
+        );
+
+        await Assets.loadBundle(bundleIds, this._downloadProgess.bind(this));
+    }
+
+    static _downloadProgess(progessRatio) {
+        if (progessRatio === 1) {
+            console.log("Download Successfully!");
+        }
+    }
+
+    static _initScene() {
+        this.playScene = new PlayScene();
+        this.app.stage.addChild(this.playScene);
+    }
+
+    static update(delta) {
+        this.playScene.update(delta)
     }
 }
 
