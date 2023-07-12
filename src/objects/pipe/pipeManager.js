7dd637497104d8c3f6031e986730d135a34f5e38
ThucPhung <phungthuc39@gmail.com>
2023-07-12 17:29:41 +0700

    - Change Data Of Pipe Manager

--- src/objects/pipe/pipeManager.js
+++ src/objects/pipe/pipeManager.js
@@ -1,49 +1,53 @@
 import { Container } from "pixi.js";
-import { PipeTop } from "./pipeTop";
+import { PipeTop, PipeTopEvent, PipeTopd } from "./pipeTop";
 import { ContainerSpawner } from "../../spawners/containerSpawner";
 import { SpawningEvent } from "../../spawners/spawner";
 import { Game } from "../../game";
+import { PipeBottom } from "./pipeBottom";
 
 export class PipeManager extends Container {
-    constructor(dataPipes) {
+    constructor(dataPipes, num_of_pipe) {
         super();
 
         this.dataPipes = dataPipes;
-        this.pipes = [];
+        this.num_of_pipe = num_of_pipe;
+        this.pipeTops = [];
 
         this._initPipeTopSpawner();
-        this._initPipe();
+        this._initPipeTop();
 
     }
 
     update(delta) {
-        // for (let i = 0; i < this.pipes.length; i++) {
-        //     this.pipes[i].x += i * delta;
-        //     if (this.pipes[i].x > Game.windowWidth) {
-        //         this.pipeTopSpawner.despawn(this.pipes[i]);
-        //     }
-        // }
+        if (this.pipeTops) {
+            this.pipeTops[0].update(delta);
+        }
     }
 
-    _initPipe() {
-        for (let i = 0; i < 10; i++) {
+    _initPipeTop() {
+        for (let i = 0; i < 3; i++) {
             let pipeTop = this.pipeTopSpawner.spawn(this);
-            this.pipes.push(pipeTop);
-            pipeTop.x = 100;
-            pipeTop.y = 100;
+            pipeTop.setPosition(this.dataPipes[i].x, this.dataPipes[i].y_top);
+            pipeTop.setScale(this.dataPipes[i].w, this.dataPipes[i].h);
+            pipeTop.setVelocity(this.dataPipes[i].velocity);
+            pipeTop.on(PipeTopEvent.WallCollision, this.onDespawner, this);
+            pipeTop.on(PipeTopEvent.AddNewPipe, this.onSpawner, this);
+            this.pipeTops.push(pipeTop);
         }
-        // this.pipeTopSpawner.despawn(pipeTop);
-        // pipeTop.emit(SpawningEvent.Despawn);
+    }
+
+    _initPipeBottom() {
+
     }
 
     _initPipeTopSpawner() {
         this.pipeTopSpawner = new ContainerSpawner();
-        this.pipeTopSpawner.init(this.createPipeTop, 10);
+        this.pipeTopSpawner.init(this.createPipeTop, 3);
     }
 
     _initPipeBottomSpawner() {
         this.pipeBottomSpawner = new ContainerSpawner();
-        this.pipeBottomSpawner.init(this.createPipeTop, 10);
+        this.pipeBottomSpawner.init(this.createPipeTop, 3);
     }
 
     createPipeTop() {
@@ -51,5 +55,15 @@ export class PipeManager extends Container {
     }
 
     createPipeBottom() {
+        return new PipeBottom();
     }
+
+    onSpawner() {
+        this.pipeTops.shift();
+    }
+
+    onDespawner(obj) {
+        this.pipeTopSpawner.despawn(obj);
+    }
+
 }
