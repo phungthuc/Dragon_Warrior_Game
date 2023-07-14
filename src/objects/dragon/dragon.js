370d4cf2ffe2a1e5b2ae5fca9dd0c90198820aa2
ThucPhung <phungthuc39@gmail.com>
2023-07-14 13:54:46 +0700

    + Add Logic Handling In Dragon Class

---
+++ src/objects/dragon/dragon.js
@@ -0,0 +1,64 @@
+import { Container, Sprite, Texture } from "pixi.js";
+import { Collider } from "../../physics/collision/collider";
+import { ColliderTag } from "../../physics/collision/colliderTag";
+import CollisionDetector, { CollisionDetectorEvent } from "../../physics/collision/collisionDetector";
+import { InputEvent, InputManager } from "../../input/inputManager";
+import { GameConstant } from "../../gameConstant";
+
+export class Dragon extends Container {
+    constructor() {
+        super();
+
+        this.acceleration = 0;
+        this.vy = GameConstant.DRAGON_VY;
+
+        this.dragonPosition = {
+            x: null,
+            y: null
+        };
+
+        this._initSprite();
+        this._initCollider();
+
+        InputManager.emitter.on(InputEvent.SpaceUp, this.onKeyUp, this);
+
+    }
+
+    _initSprite() {
+        this.dragon = new Sprite(Texture.from("dragon_05"));
+        this.dragon.width = GameConstant.DRAGON_WIDTH;
+        this.dragon.height = GameConstant.DRAGON_HEIGHT;
+        this.dragon.position.set(GameConstant.DRAGON_X, GameConstant.DRAGON_Y);
+        this.addChild(this.dragon);
+    }
+
+    _initCollider() {
+        this.dragonCollider = new Collider(ColliderTag.Dragon, 0, 0, GameConstant.DRAGON_WIDTH * 1.25, GameConstant.DRAGON_HEIGHT * 2);
+        this.dragon.addChild(this.dragonCollider);
+        CollisionDetector.instance.addCollider(ColliderTag.Dragon, this.dragonCollider);
+        this.dragonCollider.on(CollisionDetectorEvent.Colliding, this._oncollide, this);
+    }
+
+    update(delta) {
+        this.acceleration += delta * GameConstant.GRAVITY * GameConstant.ACCELERATION_SCALE;
+        if (this.acceleration > GameConstant.MAX_ACCELERATION) {
+            this.acceleration = GameConstant.MAX_ACCELERATION
+        }
+        this.dragon.y += this.acceleration;
+
+        this.dragonPosition.x = this.dragon.x;
+        this.dragonPosition.y = this.dragon.y;
+    }
+
+    _oncollide() {
+
+    }
+
+    onKeyUp() {
+        this.acceleration = GameConstant.FORCE;
+    }
+
+    getPosition() {
+        return this.dragonPosition;
+    }
+}
