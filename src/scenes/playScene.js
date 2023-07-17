5d325ba952ad5d06236b201ad107cbfa9734584a
ThucPhung <phungthuc39@gmail.com>
2023-07-17 17:22:45 +0700

    + Update Logic Handle Play Scene

--- src/scenes/playScene.js
+++ src/scenes/playScene.js
@@ -1,4 +1,4 @@
-import { Container } from "pixi.js";
+import { Container, Text, TextStyle } from "pixi.js";
 import { UIManager, UIManagerKey } from "../objects/ui/UIManager";
 import { GameState } from "./gameState";
 import data from "../../assets/levels/level.json"
@@ -16,7 +16,18 @@ export class PlayScene extends Container {
 
         this._loadLevel();
         this._initUI();
+        this._initEmitter();
         this._initGameplay();
+
+        this.styleName = new TextStyle({
+            fill: "#022c6e",
+            fontFamily: "Times New Roman",
+            fontSize: 40,
+            fontWeight: 600
+        });
+
+        this.nameGame = new Text("Level: " + this.currentLevel, this.styleName);
+        this.addChild(this.nameGame);
     }
 
     _loadLevel() {
@@ -26,7 +37,13 @@ export class PlayScene extends Container {
 
         this.level = new LevelManager();
         this.level.loadLevel(this.levelData);
+        this.level.on(GameState.LossLevel, this._onStopLoss, this);
+        this.level.on(GameState.WinLevel, this._onStopWin, this);
+    }
 
+    createLevel() {
+        this._loadLevel();
+        this._initGameplay();
     }
 
     _onKeyup() {
@@ -48,19 +65,37 @@ export class PlayScene extends Container {
         this.uIManager = new UIManager();
         this.addChild(this.uIManager);
         this.uIManager.show(GameState.Start);
+    }
 
+    _initEmitter() {
         this.uIManager.on(UIManagerKey.Playing, () => {
             this.uIManager.show(GameState.Playing);
             this._onStart();
             this.gameState = GameState.Playing;
         });
-    }
 
-    _onEnemyRemoved() {
+        this.uIManager.on(UIManagerKey.Replaying, () => {
+            this.level.reset();
+            this.uIManager.show(GameState.Playing);
+            this.createLevel();
+            this._onStart();
+            this.gameState = GameState.Playing;
+        });
 
+        this.uIManager.on(UIManagerKey.NextPlaying, () => {
+            if (this.currentLevel < 3) {
+                this.currentLevel += 1;
+                this.level.reset();
+                this.uIManager.show(GameState.Playing);
+                this.createLevel();
+                this._onStart();
+                this.gameState = GameState.Playing;
+            }
+        });
     }
 
     update(delta) {
+        this.nameGame.text = "Level: " + this.currentLevel;
         if (this.gameState === GameState.Playing) {
             CollisionDetector.instance.update(delta);
             this.level.update(delta);
@@ -69,10 +104,17 @@ export class PlayScene extends Container {
 
     _onStart() {
         this.gamePlay.visible = true;
+        this.uIManager.show(GameState.Playing);
     }
 
-    _onStop() {
+    _onStopLoss() {
+        this.gamePlay.visible = false;
+        this.uIManager.show(GameState.LossLevel);
+    }
 
+    _onStopWin() {
+        this.gamePlay.visible = false;
+        this.uIManager.show(GameState.WinLevel);
     }
 
     _onEnd() {
