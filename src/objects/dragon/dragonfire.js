ca3f97a869716c0bb1478e33886959aaef2689c8
ThucPhung <phungthuc39@gmail.com>
2023-07-17 17:26:59 +0700

    - Change Logic Handle Particles Dragon Fire

--- src/objects/dragon/dragonfire.js
+++ src/objects/dragon/dragonfire.js
@@ -1,10 +1,10 @@
 import { Container, ParticleContainer, Sprite, Texture } from "pixi.js";
-import * as particles from "@pixi/particle-emitter";
-import * as  particleSettings from "../../../assets/images/dragon/emitter.json";
 import { Collider } from "../../physics/collision/collider";
 import { ColliderTag } from "../../physics/collision/colliderTag";
 import CollisionDetector, { CollisionDetectorEvent } from "../../physics/collision/collisionDetector";
 import { GameConstant } from "../../gameConstant";
+import { ParticleController } from "../particle/particleController";
+import { SpawningEvent } from "../../spawners/spawner";
 
 export const DragonFireEvent = Object.freeze({
     Colliding: "dragonfireevent:colliding"
@@ -14,10 +14,17 @@ export class DragonFire extends Container {
     constructor() {
         super();
 
+        this.v = 0;
+
         this._initSprite();
         this._initCollider();
+        this._initParticle();
 
-        this.v = 0;
+        this.isCollide = true;
+
+        this.on(SpawningEvent.Despawn, () => {
+            this.dragonFireCollider.enable = false;
+        });
     }
 
     _initSprite() {
@@ -27,42 +34,54 @@ export class DragonFire extends Container {
         this.sprite.height = GameConstant.DRAGON_FIRE_HEIGHT;
 
         this.addChild(this.sprite);
-
-        this.particleContainer = new ParticleContainer();
-        this.addChild(this.particleContainer);
-
-        this.emitter = new particles.Emitter(
-            this.particleContainer, particleSettings
-        );
-
     }
 
     _initCollider() {
         this.dragonFireCollider = new Collider(ColliderTag.DragonFire, 0, 0, GameConstant.DRAGON_FIRE_WIDTH, GameConstant.DRAGON_FIRE_HEIGHT);
+        this.dragonFireCollider.enable = false;
         this.sprite.addChild(this.dragonFireCollider);
         CollisionDetector._instance.addCollider(ColliderTag.DragonFire, this.dragonFireCollider);
         this.dragonFireCollider.on(CollisionDetectorEvent.Colliding, this._onCollide, this);
     }
 
+    _initParticle() {
+        this.particleContainer = new ParticleController();
+        this.addChild(this.particleContainer);
+    }
+
     update(delta) {
         this.x += delta * this.v;
         this._checkOutOfScreen();
-    }
-
-    enableParticle() {
-        this.sprite.visible = true;
-        this.emitter.autoUpdate = true;
-        // this.emitter.updateSpawnPos(this.dragonFire.x, this.dragonFire.y);
-        this.emitter.emit = true;
+        this.checkIsCollide();
     }
 
     _checkOutOfScreen() {
         if (this.x > GameConstant.GAME_WIDTH) {
-            this._onCollide();
+            this.emit(DragonFireEvent.Colliding, this);
         }
     }
 
     _onCollide() {
+        this.isCollide = false;
+        this.particleContainer.enableParticle();
+        this.sprite.visible = false;
+        setTimeout(() => {
+            this._reset();
+        }, 310);
+    }
+
+    _reset() {
+        this.particleContainer.disEnableParticle();
+        this.sprite.visible = true;
         this.emit(DragonFireEvent.Colliding, this);
     }
+
+    checkIsCollide() {
+        if (this.isCollide) {
+            this.dragonFireCollider.enable = true;
+        } else if (!this.isCollide) {
+            this.dragonFireCollider.enable = false;
+        }
+    }
+
 }
