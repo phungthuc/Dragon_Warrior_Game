a81e4e97a3f6de9138a9490d9ae14aeaa2fd6e2c
ThucPhung <phungthuc39@gmail.com>
2023-07-17 17:23:37 +0700

    * Change Data Game State

--- src/scenes/gameState.js
+++ src/scenes/gameState.js
@@ -1,7 +1,7 @@
 export const GameState = Object.freeze({
     Start: "gamestate:start",
     Playing: "gamestate:playing",
-    WinLevel: "gamestate:losslevel",
+    WinLevel: "gamestate:winlevel",
     LossLevel: "gamestate:losslevel",
     Win: "gamestate:win",
     Loss: "gamestate:loss"
