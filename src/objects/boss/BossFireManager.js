b7054c156f0b4c0dbaed031bee845218d5438f74
ThucPhung <phungthuc39@gmail.com>
2023-07-17 17:16:05 +0700

    + Create Boss Fire Manager

---
+++ src/objects/boss/BossFireManager.js
@@ -0,0 +1,47 @@
+import { Container } from "pixi.js";
+import { BossFire } from "./bossFire";
+import { GameConstant } from "../../gameConstant";
+
+export const BossFireManagerEvent = Object.freeze({
+    Colliding: "bossfiremanagerevent:colliding"
+});
+
+export class BossFireManager extends Container {
+    constructor() {
+        super();
+
+        this.bossFires = [];
+        this.v = 0;
+        this.vX = 0;
+
+        this.isCollide = true;
+
+        this._init();
+    }
+
+    _init() {
+        for (let i = 0; i < GameConstant.BOSS_FIRE_QUANTITY; i++) {
+            i % 2 == 0 ? this.bossFire = new BossFire(GameConstant.BOSS_FIRE_ANGLE * i) :
+                this.bossFire = new BossFire(- GameConstant.BOSS_FIRE_ANGLE * i)
+            this.addChild(this.bossFire);
+            this.bossFires.push(this.bossFire);
+        }
+    }
+
+    update(delta) {
+        for (let i = 0; i < GameConstant.BOSS_FIRE_QUANTITY; i++) {
+            if (i % 2 == 0) {
+                this.bossFires[i].x -= this.v * delta;
+                this.bossFires[i].y -= GameConstant.BOSS_FIRE_VY * i * delta;
+            } else {
+                this.bossFires[i].x -= this.v * delta;
+                this.bossFires[i].y -= -GameConstant.BOSS_FIRE_VY * i * delta;
+            }
+        }
+    }
+
+    _onCollide() {
+
+    }
+
+}
