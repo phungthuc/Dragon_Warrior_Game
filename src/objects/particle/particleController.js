add9cee67cfc291dd45d7c9686ec51a38b4317d0
ThucPhung <phungthuc39@gmail.com>
2023-07-19 23:11:37 +0700

    + Update Logic Handle Particles

--- src/objects/particle/particleController.js
+++ src/objects/particle/particleController.js
@@ -1,32 +1,36 @@
 import { Container, ParticleContainer, Texture } from "pixi.js";
 import * as particles from "@pixi/particle-emitter";
 import * as  particleSettings from "../../../assets/images/dragon/emitter.json";
+import { SpawningEvent } from "../../spawners/spawner";
 
 export class ParticleController extends ParticleContainer {
     constructor() {
         super();
 
         this._initParticle();
+        this.on(SpawningEvent.Spawn, () => {
+            this.enableParticle();
+        });
     }
 
     _initParticle() {
         this.texture = Texture.from("particle");
         this.emitter = new particles.Emitter(this, particles.upgradeConfig(particleSettings, [this.texture]));
+        this.emitter.autoUpdate = true;
     }
 
     enableParticle() {
-        this.emitter.autoUpdate = true;
         this.emitter.emit = true;
-        this.emitter.playOnce();
+        this.emitter.playOnce(() => {
+            this.emit(SpawningEvent.Despawn, this);
+        });
     }
 
     disEnableParticle() {
-        this.emitter.autoUpdate = false;
         this.emitter.emit = false;
         this.emitter.cleanup();
     }
 
     update(dt) {
-        // this.emitter.update(dt);
     }
 }
